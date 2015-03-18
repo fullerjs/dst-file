7992db7d0c873c39d390cc2a9a8f1dda5e9bc276
Alexey Novikov <velocityzen@gmail.com>
2015-03-18 16:09:25 -0400

    New plugin api

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 "use strict";
-let fsu = require("fsu");
-let path = require("path");
 
-let DstFile = function(fuller, options) {
-	fuller.bind(this);
-	this.dst = options.dst;
+module.exports = function(f, mat, options, next) {
+	mat.write(options);
+	next(null, mat);
 };
-
-DstFile.prototype.build = function(stream, master) {
-	let self = this,
-		dst = path.join(this.dst, master);
-
-	let next = fsu.createWriteStreamUnique(dst, {force: true})
-		.on("error", function(err) {
-			self.error(err.toString().replace("Error:", ""));
-		});
-
-	return stream.pipe(next);
-};
-
-
-module.exports = DstFile;
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Destination file tool for Fuller #
 **Build everything with right tool**
+
 [For more info about fuller](https://github.com/fullerjs/fuller)
 
 Saves input stream to the file.
